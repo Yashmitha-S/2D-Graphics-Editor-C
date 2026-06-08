e06c70c9cf08c1c648818a9542f707c6f5eba3dd
Yashmitha <s.yashmitha26gmail.com@Yashmithas-MacBook-Neo.local>
2026-06-08 20:15:34 +0530

    Created canvas and display functionality

---
+++ graphics_editor.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+#define WIDTH 40
+#define HEIGHT 15
+
+char canvas[HEIGHT][WIDTH];
+
+void clearCanvas() {
+    for(int i = 0; i < HEIGHT; i++) {
+        for(int j = 0; j < WIDTH; j++) {
+            canvas[i][j] = '_';
+        }
+    }
+}
+
+void displayCanvas() {
+    for(int i = 0; i < HEIGHT; i++) {
+        for(int j = 0; j < WIDTH; j++) {
+            printf("%c", canvas[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+int main() {
+    clearCanvas();
+    displayCanvas();
+    return 0;
+}
